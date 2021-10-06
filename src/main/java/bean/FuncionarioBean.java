7477c915927e70a3323a0bd22d0e6f1f8d7eeb37
lucas-antonio2011@hotmail.com <lucas-antonio2011@hotmail.com>
2021-10-05 22:46:29 -0300

    funcionamento da tela completo

--- src/main/java/bean/FuncionarioBean.java
+++ src/main/java/bean/FuncionarioBean.java
@@ -1,13 +1,47 @@
 package bean;
 
+import javax.annotation.PostConstruct;
+import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.RequestScoped;
+import javax.faces.bean.ViewScoped;
+import javax.faces.context.FacesContext;
+import javax.inject.Named;
+
+import dao.FuncionarioDao;
+import domain.Funcionario;
+import lombok.Data;
 
 @ManagedBean(name = "Funcionario",eager = true)
-@RequestScoped
+@Named
+@ViewScoped
+@Data
 public class FuncionarioBean {
 	
+	private Funcionario funcionario;
+	private FuncionarioDao funcionarioDao;
+	
+	@PostConstruct
+	public void iniciar() {
+		funcionarioDao = new FuncionarioDao();
+		funcionario = new Funcionario();
+		
+	}
+	public void limpar() {
+		funcionario = new Funcionario();
+	}
 	public void salvar() {
-		System.out.println("botao salvar foi pressionado");
+
+		
+        FacesContext context = FacesContext.getCurrentInstance();
+        
+        
+        funcionarioDao.salvar(funcionario);
+        limpar();
+        
+        
+        FacesMessage mensagem = new FacesMessage("Funcionario Salvo");
+        mensagem.setSeverity(FacesMessage.SEVERITY_ERROR);
+        context.addMessage(null, mensagem);
 	}
 }
